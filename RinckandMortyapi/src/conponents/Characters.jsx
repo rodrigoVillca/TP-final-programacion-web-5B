c472e4f1d4b22443c8d33d2d1a7b5428224cfa4e
Rodrigo Villca <villcarodrigo70@gmail.com>
2023-09-26 17:10:39 -0300

    avanze un poco mas

--- RinckandMortyapi/src/conponents/Characters.jsx
+++ RinckandMortyapi/src/conponents/Characters.jsx
@@ -1,9 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export function Characters() {
+export function Char() {
+  const [personajes, setPersonajes] = useState([]);
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
       .then((res) => res.json())
       .then((data) => console.log(data));
   }, []);
+
+  return (
+    <ul>
+      {personajes.map((item, index) => (
+        <li key={index}>
+          <h3>{item.name}</h3>
+          <p>{item.status}</p>
+          <img src={item.img} />
+        </li>
+      ))}
+    </ul>
+  );
 }
+//meterle css
